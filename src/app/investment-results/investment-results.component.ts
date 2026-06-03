1ceab9828e4054d562de540dcfd6cd68897f1bbb
bwoodhoutson <31172910+bwoodhoutson@users.noreply.github.com>
2026-06-02 21:30:55 -0500

    91 videos - section 4 done

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,21 +1,23 @@
-import { Component, Input } from '@angular/core';
-import { CurrencyPipe } from "@angular/common";
+import { Component, inject, computed } from '@angular/core';
+import { InvestmentService } from '../investment.service';
 
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  @Input() results?: {
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[];   // (?)if we get a "results", it will be an array of objects with the shape defined by the curly brackets 
+  // @Input() results?: {
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[];   // (?)if we get a "results", it will be an array of objects with the shape defined by the curly brackets 
+
+  private investmentService = inject(InvestmentService);
+
+  results = computed(() => this.investmentService.resultsData());
 }
