d4335761dbde7b4c4d6492749ece2f6edbdf4768
bwoodhoutson <31172910+bwoodhoutson@users.noreply.github.com>
2026-06-05 23:10:13 -0500

    retry project_2 commit

--- src/investment-results.ts
+++ src/investment-results.ts
@@ -1,6 +1,7 @@
 // Use the below code as a help
 // e.g., integrate it into a service or component
 // You may need to tweak it, depending on where and how you use it
+// retry 6/5/26
 
 function calculateInvestmentResults() {
   const annualData = [];
